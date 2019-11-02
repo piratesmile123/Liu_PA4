d8cfc923526a690fea08cbda68dcb0b468423a3f
Jia <jialiu907@gmail.com>
2019-11-02 17:09:51 -0400

    My first commit

---
+++ Liu_P1/src/Application.java
@@ -0,0 +1,9 @@
+public class Application
+{
+    public static void main(String[] args)
+    {
+        DuplicateRemover dp = new DuplicateRemover();
+        dp.remove("problem1.txt");
+        dp.write("unique_words.txt");
+    }
+}
---
+++ Liu_P2/src/Application.java
@@ -0,0 +1,11 @@
+public class Application
+{
+    public static void main(String[] args)
+    {
+        String inFilename = "problem2.txt";
+        String outFilename = "unique_word_counts.txt";
+        DuplicateCounter dc = new DuplicateCounter();
+        dc.count(inFilename);
+        dc.write(outFilename);
+    }
+}
